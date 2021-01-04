8cbb872edcd4fdcaa04530f6497d4d0bc2c03ebd
Jibble Einux <50579963+JibbleEinuxCODE@users.noreply.github.com>
2021-01-04 17:33:40 +0600

    login, registration

--- app/src/main/java/com/example/nectar/SignUpActivity.java
+++ app/src/main/java/com/example/nectar/SignUpActivity.java
@@ -1,26 +1,42 @@
 package com.example.nectar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jaeger.library.StatusBarUtil;
 
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private TextView signInTV;
-    private TextInputLayout textNameTI,textPhoneTI,textPassTI,textCPassTI;
+    private TextInputLayout textNameTI,textEmailTI,textPassTI,textCPassTI;
     private Button signUpBtn;
 
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    //image picked uri
+    private Uri image_uri;
 
+    private double latitude = 0.0, longitude=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +46,15 @@ public class SignUpActivity extends AppCompatActivity {
 
         signInTV = findViewById(R.id.signInTV);
         textNameTI = findViewById(R.id.textNameTI);
-        textPhoneTI = findViewById(R.id.textPhoneTI);
+        textEmailTI = findViewById(R.id.textEmailTI);
         textPassTI = findViewById(R.id.textPassTI);
         textCPassTI = findViewById(R.id.textCPassTI);
         signUpBtn = findViewById(R.id.signUpBtn);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
 
         signInTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,9 +69,9 @@ public class SignUpActivity extends AppCompatActivity {
                 confirmInput();
             }
         });
-    }
 
-    String name,phone,password,cPassword;
+    }
+    private String name, shopName, phoneNumber, deliveryFee, country, state,city,address,email,password,cPassword;
 
     private boolean validateName(){
         name = textNameTI.getEditText().getText().toString().trim();
@@ -65,14 +85,14 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    private boolean validatePhone(){
-        phone = textPhoneTI.getEditText().getText().toString().trim();
-        if (!Patterns.PHONE.matcher(phone).matches()){
-            textPhoneTI.setError("Phone Number not valid");
+    private boolean validateEmail(){
+        email = textEmailTI.getEditText().getText().toString().trim();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            textEmailTI.setError("Email not valid");
             return false;
         }
         else {
-            textPhoneTI.setError(null);
+            textEmailTI.setError(null);
             return true;
         }
     }
@@ -80,12 +100,12 @@ public class SignUpActivity extends AppCompatActivity {
         password = textPassTI.getEditText().getText().toString().trim();
         cPassword = textCPassTI.getEditText().getText().toString().trim();
         if (password.isEmpty()) {
-            textPassTI.setError("Phone can't be empty");
+            textPassTI.setError("Password can't be empty");
             return false;
         } else {
             textPassTI.setError(null);
             if (!password.equals(cPassword)) {
-                textPassTI.setError("Password not match");
+                textCPassTI.setError("Password not match");
                 return false;
             } else {
                 textCPassTI.setError(null);
@@ -95,16 +115,80 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void confirmInput() {
-        if (!validateName() | !validatePhone() | !validatePassword()){
+        if (!validateName() | !validateEmail() | !validatePassword()){
             return;
         }
         else {
-            String Vphone = textPhoneTI.getEditText().getText().toString().trim();
-            Intent intent = new Intent(SignUpActivity.this,VerifyPhoneActivity.class);
-            intent.putExtra("Name",name);
-            intent.putExtra("Phone",Vphone);
-            intent.putExtra("Password",password);
-            startActivity(intent);
+            createAccount();
         }
+    }
+
+    private void createAccount() {
+        progressDialog.setMessage("Creating Account.");
+        progressDialog.show();
+
+        //ceate Account
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //Account Created
+                        saveFirebaseDatabase();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //failed to create account
+                progressDialog.dismiss();
+                Toast.makeText(SignUpActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void saveFirebaseDatabase() {
+        progressDialog.setMessage("Saving Account Info.");
+        final String timestamp = "" + System.currentTimeMillis();
+
+
+        //setup data to save
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("uid",""+firebaseAuth.getUid());
+        hashMap.put("email",""+ email);
+        hashMap.put("name",""+ name);
+        hashMap.put("phone","");
+        hashMap.put("country","");
+        hashMap.put("state","");
+        hashMap.put("city","" );
+        hashMap.put("address","" );
+        hashMap.put("latitude",""+ latitude);
+        hashMap.put("longitude",""+ longitude);
+        hashMap.put("timestamp",""+ timestamp);
+        hashMap.put("online","true");
+        hashMap.put("accountType","User");
+        hashMap.put("profileImage","");
+
+        //save to db
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseAuth.getUid()).setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //db updated
+                        progressDialog.dismiss();
+                        Intent intent = new Intent(SignUpActivity.this,UploadAvaterActivity.class);
+                        intent.putExtra("uid",firebaseAuth.getUid());
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed to update db
+                        progressDialog.dismiss();
+                        startActivity( new Intent(SignUpActivity.this,MainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
